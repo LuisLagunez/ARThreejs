13306c21b1b1e493b803c4f4f9af16a5ab172f6f
Luis <A20216349@alumnos.uady.mx>
2023-05-29 23:58:43 -0600

    s

--- PruebaRA/script.js
+++ PruebaRA/script.js
@@ -1,56 +1,55 @@
-import * as THREE from 'three';
+const THREE = window.THREE;
 
 const scene = new THREE.Scene();
 const camera = new THREE.Camera();
-scene.add(camera)
+scene.add(camera);
 const renderer = new THREE.WebGLRenderer({
     antialias:true,
     alpha:true
 });
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-var ArToolkitSource = new THREEx.ArToolkitSource({
+var arToolkitSource = new THREEx.ArToolkitSource({
     sourceType: "webcam",
 });
-ArToolkitSource.init(function(){
+arToolkitSource.init(function(){
     setTimeout(function(){
-        ArToolkitSource.onResizeElement();
-        ArToolkitSource.copyElementSizeTo(renderer.domElement);
-    },2000)
+        arToolkitSource.onResizeElement();
+        arToolkitSource.copyElementSizeTo(renderer.domElement);
+    }, 2000);
 });
 
-var ArToolkitSource = new THREEx.ArToolkitContext({
+var arToolkitContext = new THREEx.ArToolkitContext({
     cameraParametersUrl: 'camera_para.dat',
     detectionMode: 'color_and_matrix',
 });
-ArToolkitContext.init(function(){
-    camera.projectionMatrix.copy(ArToolkitContext.getProjectionMatrix());
+arToolkitContext.init(function(){
+    camera.projectionMatrix.copy(arToolkitContext.getProjectionMatrix());
 });
 
-var ArMarkerControls = new THREEx.ArMarkerControls(ArToolkitContext, camera,
-    {
+var arMarkerControls = new THREEx.ArMarkerControls(arToolkitContext, camera, {
     type:'pattern',
     patternUrl:'pattern-R.patt',
     changeMatrixMode:'cameraTransformMatrix'
 });
 scene.visible = false;
 
-const geometry = new THREE.CubeGeometry( 1, 1, 1 );
+const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshNormalMaterial({
     transparent: true,
     opacity: 0.5,
     side: THREE.DoubleSide 
 });
-const cube = new THREE.Mesh( geometry, material );
+const cube = new THREE.Mesh(geometry, material);
 cube.position.y = geometry.parameters.height / 2;
-scene.add( cube );
+scene.add(cube);
 
 function animate() {
-	requestAnimationFrame( animate );
-    ArToolkitContext.update(ArToolkitSource.domElement);
+    requestAnimationFrame(animate);
+    arToolkitContext.update(arToolkitSource.domElement);
     scene.visible = camera.visible;
-	renderer.render( scene, camera );
-};
+    renderer.render(scene, camera);
+}
 
 animate();
